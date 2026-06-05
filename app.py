02b8f2b0002c6f54d77a651c761af6fdc574ad75
amarioncdiving <amarion@c-diving.com>
2026-06-05 13:03:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,5 @@ def health():
     }
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("HTTP_PLATFORM_PORT", os.environ.get("PORT", 8000)))
+    app.run(host="0.0.0.0", port=port)
